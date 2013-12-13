aec34d7ab05c5744344906003dc9ffdfdd268354
Cody Mallery <cmallery@comcast.net>
2013-12-13 10:57:34 -0600

    commit

--- src/github.com/cmm101/hello/hello.go
+++ src/github.com/cmm101/hello/hello.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/cmm101/multifile"
 )
 
 func main() {
 	fmt.Printf("Hello, World!")
 	fmt.Printf("Testing Commit")
 	fmt.Printf("Test")
-
+	multifile.HelloWorld()
+	multifile.HelloWorld()
 }
---
+++ src/github.com/cmm101/multifile/multifile.go
@@ -0,0 +1,11 @@
+package multifile
+
+import (
+	"fmt"
+	"math"
+)
+
+func HelloWorld() {
+	fmt.Println("\nHello world From File 2.")
+	fmt.Println(math.Abs(30))
+}
